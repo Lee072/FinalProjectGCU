4c807f8b3e73c98795af344a8d4cca02cee2fac3
MmouseLee <mia.lee2076@gmail.com>
2023-11-16 08:15:39 -0500

    New Method

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('.switch').click(() => {
+    $([".light [class*='-light']", ".dark [class*='-dark']"]).each((i, ele) => {
+        $(ele).toggleClass('bg-light bg-dark')
+        $(ele).toggleClass('text-light text-dark')
+        $(ele).toggleClass('navbar-light navbar-dark')
+        $(ele).toggleClass('div-light div-dark')
+    })
+    // toggle body class selector
+    $('body').toggleClass('light dark')
+})
