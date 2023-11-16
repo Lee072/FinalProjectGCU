34af78fb570262b37c9092487de5669f6018bb04
MmouseLee <mia.lee2076@gmail.com>
2023-11-16 10:43:21 -0500

    Staring Dark Mode

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,13 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$('.switch').click(() => {
-    $([".light [class*='-light']", ".dark [class*='-dark']"]).each((i, ele) => {
-        $(ele).toggleClass('bg-light bg-dark')
-        $(ele).toggleClass('text-light text-dark')
-        $(ele).toggleClass('navbar-light navbar-dark')
-        $(ele).toggleClass('div-light div-dark')
-    })
-    // toggle body class selector
-    $('body').toggleClass('light dark')
-})
+function darkModeToggle() {
+    element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
