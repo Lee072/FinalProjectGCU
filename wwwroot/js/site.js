6353c32221b11375aa8daa40227eaa23548bf780
MmouseLee <mia.lee2076@gmail.com>
2023-11-21 09:19:36 -0500

    Trying to get dark mode on all pages

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,8 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+const { Checkbox } = require("../../../../../node_modules/@mui/material/index");
+
 // Write your JavaScript code.
 function darkModeToggle() {
     var element = document.body;
@@ -8,4 +10,7 @@ function darkModeToggle() {
     element.classList.toggle("navbar-dark");
     element.classList.toggle("headers-dark");
 }
+
+
+
 
